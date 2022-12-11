65e0eb6ec8af74f450009917efa2b90d03ff9bbc
Anders Fylling <anders@nordic.email>
2022-12-11 03:22:09 +0100

    delete dead intents code

--- intent/intents_test.go
+++ intent/intents_test.go
@@ -6,20 +6,6 @@ import (
 	"github.com/discordpkg/gateway/event"
 )
 
-func TestAllAndSum(t *testing.T) {
-	all := All()
-	sum := Sum
-
-	sumAll := Type(0)
-	for i := range all {
-		sumAll += all[i]
-	}
-
-	if sum != sumAll {
-		t.Fatal(`"Sum" does not have the same value as sum of "All()"`)
-	}
-}
-
 func TestDMEventsToIntents(t *testing.T) {
 	type test struct {
 		intent Type
